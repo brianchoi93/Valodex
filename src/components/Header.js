1ad2e34bc800422defa8c4aeeac79a6d4a1f751f
brianchoi93 <brianchoi93@gmail.com>
2022-06-18 01:40:01 -0700

    add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 function Header(props) {
   return (
-    <div>
-      header
-    </div>
+    <img src="https://fontmeme.com/permalink/220618/a59e6ab8cb7ca18556242da1997b8374.png" alt="" />
   );
 }
 
