23810eb6968cdcfd387e71c3b94cae5a9b2aa371
brianchoi93 <brianchoi93@gmail.com>
2022-06-18 01:45:24 -0700

    Add nav with css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import './Nav.css'
 
 function Nav(props) {
   return (
-    <div>
-      nav
-    </div>
+    <ul id="navbar">
+      <li className="navlist">Home</li>
+      <li className="navlist">Agents</li>
+      <li className="navlist">Weapons</li>
+      <li className="navlist">Maps</li>  
+    </ul>
   );
 }
 
